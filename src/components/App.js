0a40794bdb486e0e9f6b64b36c97490a7fa842a2
atielijohn8 <atielijohn4@gmail.com>
2025-07-31 20:00:29 +0300

    list-hooks

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,15 @@ import About from "./About";
 import ProjectList from "./ProjectList";
 import user from "../data/user";
 
+// This is the main App component that renders the entire application 
 function App() {
   return (
     <div>
+       {/* Navigation bar at the top containing home, about and projects */}
+       
       <NavBar />
+
+      {/* Home section displaying a user's name, city, and favorite color */}
       <Home username={user.name} city={user.city} color={user.color} />
       <About bio={user.bio} links={user.links} />
       <ProjectList projects={user.projects} />
